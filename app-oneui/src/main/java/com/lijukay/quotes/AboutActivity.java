36ac62e944079f2d93177513c7253ed0642a389e
yanndroid <yanndroid.dev@gmail.com>
2023-07-15 02:22:51 +0200

    app-oneui: imported from template, adjusted for Qwotable

---
+++ app-oneui/src/main/java/com/lijukay/quotes/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.lijukay.quotes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app-oneui/src/main/java/com/lijukay/quotes/MainActivity.java
@@ -0,0 +1,47 @@
+package com.lijukay.quotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.oneuiproject.oneui.layout.ToolbarLayout;
+import dev.oneuiproject.oneui.utils.ActivityUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToolbarLayout toolbarLayout = findViewById(R.id.toolbar_layout);
+        toolbarLayout.setNavigationButtonAsBack();
+
+        /*TODO:
+         *  - change packageName and name
+         *  - check other TODOs
+         * */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_app_info) {
+            ActivityUtils.startPopOverActivity(this,
+                    new Intent(this, AboutActivity.class),
+                    null,
+                    ActivityUtils.POP_OVER_POSITION_RIGHT | ActivityUtils.POP_OVER_POSITION_TOP);
+            return true;
+        }
+        return false;
+    }
+}
