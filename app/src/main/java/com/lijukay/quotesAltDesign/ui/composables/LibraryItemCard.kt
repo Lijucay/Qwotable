83a294916012870772c9155ee1ecf7e0f5b07641
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-07 22:35:29 +0100

    Ctrl + Alt + Shift + L on this file

--- app/src/main/java/com/lijukay/quotesAltDesign/ui/composables/LibraryItemCard.kt
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/composables/LibraryItemCard.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
