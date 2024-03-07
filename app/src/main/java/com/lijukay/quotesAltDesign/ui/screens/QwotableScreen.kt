de9b5ae0361e3a59740214e9fec96e99cf1e6ebc
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-07 22:40:26 +0100

    Replaced the QwotableList in the Screen with the actual QwotableList from QwotableLists.kt

--- app/src/main/java/com/lijukay/quotesAltDesign/ui/screens/QwotableScreen.kt
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/screens/QwotableScreen.kt
@@ -1,19 +1,12 @@
 package com.lijukay.quotesAltDesign.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.Observer
-import com.lijukay.core.database.Qwotable
 import com.lijukay.core.utils.QwotableViewModel
-import com.lijukay.quotesAltDesign.ui.composables.QwotableItemCard
+import com.lijukay.quotesAltDesign.ui.composables.QwotableList
 import com.lijukay.quotesAltDesign.ui.theme.QwotableTheme
 
 
@@ -26,34 +19,6 @@ fun QwotableScreen(qwotableViewModel: QwotableViewModel, modifier: Modifier = Mo
             color = MaterialTheme.colorScheme.background
         ) {
             QwotableList(qwotableViewModel = qwotableViewModel)
-        }
-    }
-}
-
-
-@Composable
-private fun QwotableList(qwotableViewModel: QwotableViewModel, modifier: Modifier = Modifier) {
-    val qwotableState = remember { mutableStateOf(emptyList<Qwotable>()) }
-
-    DisposableEffect(key1 = qwotableViewModel) {
-        val observer = Observer { qwotables: List<Qwotable> ->
-            qwotableState.value = qwotables
-        }
-
-        qwotableViewModel.observedQwotables.observeForever(observer)
-
-        onDispose {
-            qwotableViewModel.observedQwotables.removeObserver(observer)
-        }
-    }
-
-    val rememberQwotableState = remember(qwotableState) { qwotableState }
-
-    LazyColumn(modifier = modifier
-        .fillMaxSize()
-    ) {
-        items(rememberQwotableState.value) { qwotable: Qwotable ->
-            QwotableItemCard(qwotable = qwotable)
         }
     }
 }
