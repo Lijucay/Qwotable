014bb7effebbc351ee21b0849decf14ae84a7a94
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-07 22:37:40 +0100

    Added FloatingActionButton and OwnQwotableList to the OwnQwotablesScreen

--- app/src/main/java/com/lijukay/quotesAltDesign/ui/screens/OwnQwotablesScreen.kt
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/screens/OwnQwotablesScreen.kt
@@ -1,8 +1,38 @@
 package com.lijukay.quotesAltDesign.ui.screens
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.lijukay.core.utils.QwotableViewModel
+import com.lijukay.quotesAltDesign.R
+import com.lijukay.quotesAltDesign.ui.composables.OwnQwotableList
 
 @Composable
-fun OwnQwotablesScreen(qwotableViewModel: QwotableViewModel) {
+fun OwnQwotablesScreen(modifier: Modifier = Modifier, qwotableViewModel: QwotableViewModel) {
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(imageVector = Icons.Rounded.Edit, contentDescription = null)
+                Text(
+                    text = stringResource(id = R.string.edit_qwotable),
+                    modifier = modifier.padding(start = 8.dp)
+                )
+            }
+        }
+    ) { paddingValues ->
+        OwnQwotableList(
+            qwotableViewModel = qwotableViewModel,
+            modifier = modifier.padding(paddingValues)
+        )
+    }
 }
