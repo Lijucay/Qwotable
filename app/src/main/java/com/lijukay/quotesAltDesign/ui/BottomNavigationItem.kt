6541a1d03646a9df3c063239f0deac3913036c2a
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-07 22:32:37 +0100

    Renamed bottomNavigationIcons to bottomNavigationItems Changed all filled icons by rounded icons (they are filled by default)

--- app/src/main/java/com/lijukay/quotesAltDesign/ui/BottomNavigationItem.kt
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/BottomNavigationItem.kt
@@ -1,32 +1,38 @@
 package com.lijukay.quotesAltDesign.ui
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.lijukay.core.Screens
 
 data class BottomNavigationItem(
     val label: String = "",
-    val icon: ImageVector = Icons.Filled.Home,
+    val icon: ImageVector = Icons.Rounded.Home,
     val route: String = ""
 ) {
-    fun bottomNavigationIcons(): List<BottomNavigationItem> {
+    fun bottomNavigationItems(): List<BottomNavigationItem> {
         return listOf(
             BottomNavigationItem(
                 label = "Home",
-                icon = Icons.Filled.Home,
+                icon = Icons.Rounded.Home,
                 route = Screens.Home.route
             ),
             BottomNavigationItem(
+                label = "Qwotables",
+                icon = Icons.Rounded.Star,
+                route = Screens.Qwotable.route
+            ),
+            BottomNavigationItem(
                 label = "Favorites",
-                icon = Icons.Filled.Favorite,
+                icon = Icons.Rounded.Favorite,
                 route = Screens.Favorite.route
             ),
             BottomNavigationItem(
                 label = "My Qwotables",
-                icon = Icons.Filled.Person,
+                icon = Icons.Rounded.Person,
                 route = Screens.OwnQwotables.route
             )
         )
