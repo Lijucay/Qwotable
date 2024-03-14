14c2a4931e703807282b597a1474bb13ca0ff419
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-14 19:50:13 +0100

    Removed unnecessary color declarations

--- app/src/main/java/com/lijukay/quotesAltDesign/ui/theme/Theme.kt
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/theme/Theme.kt
@@ -15,18 +15,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val DarkColorScheme = darkColorScheme()
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
+    /* Default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
