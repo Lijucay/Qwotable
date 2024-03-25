9fdcf4c68455f3956f584a85312ca5a57d4d5f54
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-26 00:15:03 +0100

    Adding dialog for basic information

---
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/dialogs/InformationDialog.kt
@@ -0,0 +1,92 @@
+/*
+* Copyright (C) 2024 Lijucay (Luca)
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>
+* */
+
+package com.lijukay.quotesAltDesign.ui.dialogs
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun InformationDialog(
+    modifier: Modifier = Modifier,
+    title: String,
+    message: String,
+    showCancel: Boolean,
+    onDismissRequest: () -> Unit,
+    onConfirmationRequest: () -> Unit
+) {
+    val scrollState = rememberScrollState()
+
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(decorFitsSystemWindows = false)
+    ) {
+        ElevatedCard(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(
+                    vertical = 36.dp,
+                    horizontal = 5.dp
+                ),
+            shape = RoundedCornerShape(28.dp)
+        ) {
+            Text(
+                text = title,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = message, modifier = modifier
+                .padding(16.dp)
+                .verticalScroll(scrollState))
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                if (showCancel) {
+                    TextButton(onClick = onDismissRequest) {
+                        Text(text = stringResource(id = android.R.string.cancel))
+                    }
+                }
+                TextButton(onClick = onConfirmationRequest) {
+                    Text(text = stringResource(id = android.R.string.ok))
+                }
+            }
+        }
+    }
+}
