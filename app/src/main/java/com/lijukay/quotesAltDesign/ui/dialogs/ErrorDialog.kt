5458239aa28cbd6e2338e6a7491db284c72f6da7
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-26 00:11:01 +0100

    Adding dialog for important informations (Error dialog)

---
+++ app/src/main/java/com/lijukay/quotesAltDesign/ui/dialogs/ErrorDialog.kt
@@ -0,0 +1,127 @@
+/*
+* Copyright (C) 2024 Lijucay (Luca)
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>
+* */
+
+package com.lijukay.quotesAltDesign.ui.dialogs
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ErrorOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.lijukay.quotesAltDesign.R
+
+@Composable
+fun ErrorDialog(
+    modifier: Modifier = Modifier,
+    title: String,
+    message: String,
+    onConfirmationRequest: () -> Unit,
+    onDismissRequest: () -> Unit
+    ) {
+    val scrollState = rememberScrollState()
+
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        ElevatedCard(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 5.dp,
+                    vertical = 36.dp
+                ),
+            shape = RoundedCornerShape(28.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.errorContainer
+            )
+        ) {
+            Column(
+                modifier = modifier
+                    .verticalScroll(state = scrollState)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(imageVector = Icons.Rounded.ErrorOutline, contentDescription = null)
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = modifier.height(8.dp))
+                Text(text = message)
+                Spacer(modifier = modifier.height(8.dp))
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = 8.dp,
+                            bottom = 4.dp,
+                            start = 8.dp,
+                            end = 8.dp
+                        )
+                ) {
+                    Button(
+                        onClick = { onDismissRequest() },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.errorContainer,
+                            contentColor = MaterialTheme.colorScheme.onErrorContainer
+                        ),
+                        modifier = modifier
+                            .weight(1f)
+                            .padding(end = 2.dp)
+                    ) {
+                        Text(text = stringResource(id = android.R.string.cancel))
+                    }
+                    Button(
+                        onClick = {
+                            onConfirmationRequest()
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.error,
+                            contentColor = MaterialTheme.colorScheme.onError
+                        ),
+                        modifier = modifier
+                            .weight(1f)
+                            .padding(start = 2.dp)
+                    ) {
+                        Text(text = stringResource(id = R.string.confirm))
+                    }
+                }
+            }
+        }
+    }
+}
