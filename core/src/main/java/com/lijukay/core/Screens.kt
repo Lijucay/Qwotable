8fd9382ccb3e36751b769cefa36c0019c210ec50
Lijukay (Luca) <lico.keins@gmail.com>
2024-03-26 00:21:09 +0100

    Moving classes to appropriate subpackages to improve the code space

--- core/src/main/java/com/lijukay/core/Screens.kt
+++
@@ -1,8 +0,0 @@
-package com.lijukay.core
-
-sealed class Screens(val route: String) {
-    object Home : Screens("home_route")
-    object Favorite : Screens("favorites_route")
-    object Qwotable : Screens("qwotable_route")
-    object OwnQwotables : Screens("own_qwotables_route")
-}
