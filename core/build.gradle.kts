e062319d1640f8fdc12929260735f623de42ffdf
Lijucay (Luca) <lico.keins@gmail.com>
2024-04-21 02:40:56 +0200

    Update dependencies and add dependency for datastore (yet to be used)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -47,13 +48,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.7.0")
-    implementation("androidx.fragment:fragment-compose:1.7.0-beta01")
+    implementation("androidx.fragment:fragment-compose:1.7.0-rc02")
 
     implementation("com.google.code.gson:gson:2.10.1")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
