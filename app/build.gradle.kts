fe4bb18206dbe4043ba07e7f25d75e361a9c455b
Lijucay (Luca) <lico.keins@gmail.com>
2024-06-02 18:40:49 +0200

    App and Gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lijukay.quotesAltDesign"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
