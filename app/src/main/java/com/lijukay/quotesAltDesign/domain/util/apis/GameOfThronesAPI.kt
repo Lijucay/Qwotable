c37655273a97a658f76aabda33c314eb3a4c05cd
Lijucay (Luca) <lico.keins@gmail.com>
2025-04-05 23:46:34 +0200

    Delete literally everything. Fresh start!

--- app/src/main/java/com/lijukay/quotesAltDesign/domain/util/apis/GameOfThronesAPI.kt
+++
@@ -1,9 +0,0 @@
-package com.lijukay.quotesAltDesign.domain.util.apis
-
-import com.lijukay.quotesAltDesign.data.remote.model.responses.GameOfThronesAPIResponse
-import retrofit2.http.GET
-
-interface GameOfThronesAPI {
-    @GET("v1/random")
-    suspend fun getRandomQuote(): GameOfThronesAPIResponse
-}
